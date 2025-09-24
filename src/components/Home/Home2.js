cd6384e563e532a4674315775b98a064b1c590b7
shiv1503 <90271629+shiv1503@users.noreply.github.com>
2025-09-24 10:54:54 +0530

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -67,7 +67,7 @@ function Home2() {
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/"
+                  href="https://github.com/shiv1503"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
